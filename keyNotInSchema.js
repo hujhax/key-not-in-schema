6b8302b5b61f6eaa6dfe696aaf4455b5e0e7eaa6
Peter Rogers <axyaqehsmq@snkmail.com>
2015-02-10 19:54:51 -0600

    Adding basic test.

--- keyNotInSchema.js
+++ keyNotInSchema.js
@@ -14,6 +14,23 @@ if (Meteor.isClient) {
       Session.set('counter', Session.get('counter') + 1);
     }
   });
+
+  Template.hello.rendered = function() {
+    var testSchema = new SimpleSchema({foo: {type: String}, bar: {type: Number}});
+
+    var badData = {foo: 'a', bar: 1, baz: "whoops"};
+
+    var context = testSchema.namedContext("test");
+
+    if (!(context.validate(badData))) {
+        var keys = context.invalidKeys();
+        if (!keys || !keys[0] || !keys[0].name) {
+            return;
+        }
+
+        console.log(context.keyErrorMessage(keys[0].name));
+    }
+  };
 }
 
 if (Meteor.isServer) {
